d50edc3b7ea58061be67fde38879b29817e9ba60
DOUGALS LEAL <StarPlatinun00023@outlook.com>
2026-02-09 20:49:24 -0400

    Normalizando quebras de linha para LF

--- src/PerfilModal/index.tsx
+++ src/PerfilModal/index.tsx
@@ -4,39 +4,39 @@ import { formataPreco } from '../utils/formatters'
 import { ModalContainer, Modal, BotaoModal, ModalContent } from './styles'
 
 type Props = {
-  product: Prato | null
-  isVisible: boolean
-  onClose: () => void
+    product: Prato | null
+    isVisible: boolean
+    onClose: () => void
 }
 
 const ProductModal = ({ product, isVisible, onClose }: Props) => {
-  const handleConfirm = () => {
-    onClose()
-  }
+    const handleConfirm = () => {
+        onClose()
+    }
 
-  if (!isVisible || !product) return null
+    if (!isVisible || !product) return null
 
-  return (
-    <Modal className="visivel">
-      <div className="overlay" onClick={onClose}></div>
-      <ModalContainer>
-        <img onClick={onClose} src={closeIcon} alt="ícone de fechar" />
-        <ModalContent>
-          <img src={product.foto} alt={product.nome} />
-          <div>
-            <h4>{product.nome}</h4>
-            <p>{product.descricao}</p>
-            <p>
-              Serve de <span>{product.porcao}</span>
-            </p>
-            <BotaoModal onClick={handleConfirm}>
-              Confirmar - {formataPreco(product.preco)}
-            </BotaoModal>
-          </div>
-        </ModalContent>
-      </ModalContainer>
-    </Modal>
-  )
+    return (
+        <Modal className="visivel">
+            <div className="overlay" onClick={onClose}></div>
+            <ModalContainer>
+                <img onClick={onClose} src={closeIcon} alt="ícone de fechar" />
+                <ModalContent>
+                    <img src={product.foto} alt={product.nome} />
+                    <div>
+                        <h4>{product.nome}</h4>
+                        <p>{product.descricao}</p>
+                        <p>
+                            Serve de <span>{product.porcao}</span>
+                        </p>
+                        <BotaoModal onClick={handleConfirm}>
+                            Confirmar - {formataPreco(product.preco)}
+                        </BotaoModal>
+                    </div>
+                </ModalContent>
+            </ModalContainer>
+        </Modal>
+    )
 }
 
 export default ProductModal
